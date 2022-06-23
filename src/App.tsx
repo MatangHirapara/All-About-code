5fc8401ebe58cfd197b659ae4ee1e05f40d40668
matang <matang.hirapara1@gmail.com>
2022-06-23 16:47:23 +0530

    this big commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <button>learn React</button>
+        <button>check React</button>
       </header>
     </div>
   );
---
+++ src/Continue.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Continue = () => {
+  return (
+    <div>hello child</div>
+  )
+}
+
+export default Continue
